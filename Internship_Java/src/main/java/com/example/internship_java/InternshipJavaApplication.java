4bb416ea7444686dd6b0f657eee691c110270015
laurentiu.borza <laurentiu.borza@student.upt.ro>
2022-07-29 14:05:08 +0300

    Solve the errors

--- Internship_Java/src/main/java/com/example/internship_java/InternshipJavaApplication.java
+++ Internship_Java/src/main/java/com/example/internship_java/InternshipJavaApplication.java
@@ -2,8 +2,10 @@ package com.example.internship_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.internship_java.repository")
 public class InternshipJavaApplication {
 
 	public static void main(String[] args) {
--- Internship_Java/src/main/java/com/example/internship_java/repository/InterviewTypeRepository.java
+++ Internship_Java/src/main/java/com/example/internship_java/repository/InterviewTypeRepository.java
@@ -2,7 +2,9 @@ package com.example.internship_java.repository;
 
 import com.example.internship_java.model.InterviewType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InterviewTypeRepository extends JpaRepository<InterviewType, String> {
-    InterviewType findByInterviewType(String interviewType);
+    InterviewType findByName(String interviewType);
 }
