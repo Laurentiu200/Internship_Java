164c067f4a59f5780709f367d9ac5e890dd6ea53
Laurentiu200 <74619400+Laurentiu200@users.noreply.github.com>
2022-07-29 11:21:23 +0300

    Add files via upload

---
+++ Internship_Java/src/main/java/com/example/internship_java/InternshipJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.internship_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipJavaApplication.class, args);
+	}
+
+}
---
+++ Internship_Java/src/test/java/com/example/internship_java/InternshipJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internship_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
