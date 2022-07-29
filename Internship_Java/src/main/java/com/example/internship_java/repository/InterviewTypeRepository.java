d1e61fe496b59799d723c9eec2a95e880505658d
mihai.vicol <mihai.vicol01@e-uvt.ro>
2022-07-29 13:03:26 +0300

    Implement controller

--- Internship_Java/src/main/java/com/example/internship_java/repository/InterviewTypeRepository.java
+++ Internship_Java/src/main/java/com/example/internship_java/repository/InterviewTypeRepository.java
@@ -1,4 +1,8 @@
 package com.example.internship_java.repository;
 
-public interface InterviewTypeRepository {
+import com.example.internship_java.model.InterviewType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewTypeRepository extends JpaRepository<InterviewType, String> {
+    InterviewType findByInterviewType(String interviewType);
 }
